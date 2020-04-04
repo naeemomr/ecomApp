16a91a048fc8bf9c9fe507e9800e377804d92d39
riversilica_naeemkp <naeemudhin@riversilica.com>
2020-04-04 22:01:27 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
-import { Route } from 'react-router-dom'
+import Header from './components/header/header.component.jsx'
+import { Route, Switch } from 'react-router-dom'
 
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/shop' component={ShopPage}></Route>
+      <Header />
+      <Switch>
+        <div>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+        </div>
+      </Switch>
     </div>
   );
 }
